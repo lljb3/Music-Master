c4d1721c37cce625bd70a9e5ffb0fced6a5d1728
lljb3 <lljb3@lljb3.com>
2017-11-11 21:54:48 -0600

    Added Fancybox and jPlayer

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -21,25 +21,12 @@
 <!-- Footer Information -->	
 <footer id="footer-container">
     <div class="container-fluid">
-        <div class="row" id="footer-info">
-            <div class="col-md-6 col-md-offset-1 col-xs-12">
-            	<div class="has-text pull-left">
-                	<?php echo $copyright; ?> | <a href="/terms-of-use/"><strong>Terms</strong></a> | <a href="/privacy-policy/"><strong>Privacy</strong></a> | Site by <a href="http://prodhmd.com/" target="blank"><strong>HMD Web</strong></a>
-                <!-- end .has-text --></div>
-            <!-- end .col-md-5 --></div>
-            <div class="col-md-4 col-xs-12">
-                <div class="social pull-right">
-                    <?php if ( $facebook ) { ?><a href="<?php echo $facebook; ?>"><i aria-hidden="true" class="fa fa-facebook facebook"></i></a><?php } ?>
-                    <?php if ( $twitter ) { ?><a href="<?php echo $twitter; ?>"><i aria-hidden="true" class="fa fa-twitter twitter"></i></a><?php } ?>
-                    <?php if ( $google ) { ?><a href="<?php echo $google; ?>"><i aria-hidden="true" class="fa fa-google google"></i></a><?php } ?>
-                    <?php if ( $linkedin ) { ?><a href="<?php echo $linkedin; ?>"><i aria-hidden="true" class="fa fa-linkedin linkedin"></i></a><?php } ?>
-                    <?php if ( $youtube ) { ?><a href="<?php echo $youtube; ?>"><i aria-hidden="true" class="fa fa-youtube youtube"></i></a><?php } ?>
-                    <?php if ( $pinterest ) { ?><a href="<?php echo $pinterest; ?>"><i aria-hidden="true" class="fa fa-pinterest pinterest"></i></a><?php } ?>
-                    <?php if ( $instagram ) { ?><a href="<?php echo $instagram; ?>"><i aria-hidden="true" class="fa fa-instagram instagram"></i></a><?php } ?>
-                    <?php if ( $skype ) { ?><a href="<?php echo $skype; ?>"><i aria-hidden="true" class="fa fa-skype skype"></i></a><?php } ?>
-                    <?php if ( $yelp ) { ?><a href="<?php echo $yelp; ?>"><i aria-hidden="true" class="fa fa-yelp yelp"></i></a><?php } ?>
-				<!-- end .social --></div>
-            <!-- end .col-md-5 --></div>
+        <div class="row">
+            <?php if ( is_shop() || is_woocommerce() || is_cart() || is_checkout() || is_account_page() ) : ?>
+            	<!-- No player fired -->
+            <?php else : ?>            
+                <?php require_once('player.php'); ?>
+            <?php endif; ?>
         <!-- end .row --></div>
-    <!-- end .container-fluid --></div>
+    <!-- end .hidden-xs --></div>
 <!-- end #footer-container --></footer>
--- parts/shared/header.php
+++ parts/shared/header.php
@@ -10,87 +10,87 @@
 ?>
 
 <!-- Header Information -->
-<header id="header-container">
-    <div class="container-fluid" id="header-menu">
-    	<div class="row">
-            <div class="col-md-10 col-md-offset-1" id="header-menu-table">
-                <!-- Large Menu -->
-                <div class="menu center-block hidden-xs" id="header-menu-table-row">
-                    <a href="<?php echo esc_url( home_url('/') ); ?>" class="navbar-brand pull-left" id="header-menu-table-cell">
-                        <?php if( empty( $logo ) ) { ?>
-                            <span><?php bloginfo('name'); ?></span>
-                        <?php } else { ?>
-                            <img src="<?php echo $logo; ?>" alt="" class="img-responsive">
-                        <?php } ?>
-                    <!-- end .navbar-brand --></a>
-                    <div class="navbar-collapse collapse pull-right" id="header-menu-table-cell">
-                        <?php wp_nav_menu( 
-                            array(
-                                'theme_location' => 'primary',
-                                'container'      => 'ul',
-                                'menu_class'     => 'navbar-nav nav',
-                                'echo'           => true,
-                                'link_before'    => '<span>',
-                                'link_after'     => '</span>',
-                                'items_wrap'     => '<ul class="navbar-nav nav">%3$s</ul>',
-                                'depth'          => 0,
-								'walker'		 => new wp_bootstrap_navwalker()
-                            )
-                        ); ?>
-                        <ul class="navbar-nav nav">
-                            <?php if( !empty( $chat ) ) { 
-                                echo '<script type="text/javascript">' . $chat . '</script>';
-                                echo '<li class="menu-chat menu-item"><i class="fa fa-comments-o" aria-hidden="true"></i></li>'; 
-                            } ?>
-                            <?php if( !empty( $phone ) ) { 
-                                echo '<li class="menu-phone menu-item"><a title="Call" href="tel:' . $phone . '"><i class="fa fa-mobile" aria-hidden="true" ></i></a></li>'; 
-                            } ?>
-                        <!-- end .navbar-nav --></ul>
-                    <!-- end .collapse --></div>
-                <!-- end .menu --></div>
-                <!-- Small Menu -->
-                <div class="menu center-block visible-xs">
-                    <div class="navbar-header">
-                        <a href="<?php echo esc_url( home_url('/') ); ?>" class="navbar-brand pull-left">
-                            <?php if( empty( $logo ) ) { ?>
-                                <span><?php bloginfo('name'); ?></span>
+<header id="header-container" class="row">
+    <div class="menu center-block">
+        <a href="<?php echo esc_url( home_url('/') ); ?>" class="navbar-brand center-block">
+            <?php if( empty( $logo ) ) { ?>
+                <span><?php bloginfo('name'); ?></span>
+            <?php } else { ?>
+                <img src="<?php echo $logo; ?>" alt="" class="img-responsive center-block">
+            <?php } ?>
+        <!-- end .navbar-brand --></a>
+        <button type="button" class="navbar-toggle pull-right collapsed" data-toggle="collapse" data-target="#header-collapse">
+            <span class="sr-only">Menu</span>
+            <span class="icon-bar top-bar"></span>
+            <span class="icon-bar middle-bar"></span>
+            <span class="icon-bar bottom-bar"></span>
+        <!-- end .navbar-toggle --></button>
+        <div class="panel-collapse collapse" id="header-collapse">
+            <div class="header-table">
+                <div class="container-fluid" id="header-menu">
+                    <div class="row text-center">
+                        <div class="col-md-10 col-md-offset-1">
+                            <a href="<?php echo esc_url( home_url('/home') ); ?>" class="navbar-brand center-block">
+                                <?php if( empty( $logo ) ) { ?>
+                                    <span><?php bloginfo('name'); ?></span>
+                                <?php } else { ?>
+                                    <img src="<?php echo $logo; ?>" alt="" class="img-responsive center-block">
+                                <?php } ?>
+                            <!-- end .navbar-brand --></a>
+                            <?php if( is_shop() || is_woocommerce() || is_cart() || is_checkout() || is_account_page() ) { ?>
+                                <?php wp_nav_menu( 
+                                    array(
+                                        'theme_location' => 'shop',
+                                        'container'      => 'ul',
+                                        'menu_class'     => 'navbar-nav nav',
+                                        'echo'           => true,
+                                        'link_before'    => '<span>',
+                                        'link_after'     => '</span>',
+                                        'items_wrap'     => '<ul class="navbar-nav nav">%3$s</ul>',
+                                        'depth'          => 0,
+                                        'walker'		 => new wp_bootstrap_navwalker()
+                                    )
+                                ); ?>
                             <?php } else { ?>
-                                <img src="<?php echo $logo; ?>" alt="" class="img-responsive">
+                                <?php wp_nav_menu( 
+                                    array(
+                                        'theme_location' => 'primary',
+                                        'container'      => 'ul',
+                                        'menu_class'     => 'navbar-nav nav',
+                                        'echo'           => true,
+                                        'link_before'    => '<span>',
+                                        'link_after'     => '</span>',
+                                        'items_wrap'     => '<ul class="navbar-nav nav">%3$s</ul>',
+                                        'depth'          => 0,
+                                        'walker'		 => new wp_bootstrap_navwalker()
+                                    )
+                                ); ?>
                             <?php } ?>
-                        <!-- end .navbar-brand --></a>
-                        <button type="button" class="navbar-toggle pull-right collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-                            <span class="sr-only">Menu</span>
-                            <span class="icon-bar top-bar"></span>
-                            <span class="icon-bar middle-bar"></span>
-                            <span class="icon-bar bottom-bar"></span>
-                        <!-- .navbar-toggle --></button>
-                    <!-- end .navbar-header --></div>
-                    <div class="navbar-collapse collapse text-center" id="navbar-collapse">
-                        <?php wp_nav_menu( 
-                            array(
-                                'theme_location'  => 'primary',
-                                'container'       => 'ul',
-                                'menu_class'      => 'navbar-nav nav',
-                                'echo'            => true,
-                                'link_before'     => '<span>',
-                                'link_after'      => '</span>',
-                                'items_wrap'      => '<ul class="navbar-nav nav">%3$s</ul>',
-                                'depth'           => 0,
-								'walker'          => new wp_bootstrap_navwalker()
-                            )
-                        ); ?>
-                        <ul class="navbar-nav nav">
-                            <?php if( !empty( $chat ) ) { 
-                                echo '<script type="text/javascript">' . $chat . '</script>';
-                                echo '<li class="menu-chat menu-item"><a title="Chat" onclick="' . $chat_click . '"><i class="fa fa-comments-o" aria-hidden="true"></a></i></li>'; 
-                            } ?>
-                            <?php if( !empty( $phone ) ) { 
-                                echo '<li class="menu-phone menu-item"><a title="Call" href="tel:' . $phone . '"><i class="fa fa-mobile" aria-hidden="true" ></i></a></li>'; 
-                            } ?>
-                        <!-- end .navbar-nav --></ul>
-                    <!-- end .collapse --></div>
-                <!-- end .visible-xs --></div>
-        	<!-- end .col-md-12 --></div>
-        <!-- end .row --></div>
-    <!-- end .container-fluid --></div>
+                        <!-- end .col-md-8 --></div>
+                    <!-- end .text-center --></div>
+                <!-- end .container-fluid --></div>
+                <div class="container-fluid" id="header-info">
+                    <div class="row text-center" id="site-social">
+                        <div class="col-md-10 col-md-offset-1">
+                            <?php if ( $facebook ) { ?><a href="<?php echo $facebook; ?>"><i aria-hidden="true" class="fa fa-facebook facebook"></i></a><?php } ?>
+                            <?php if ( $twitter ) { ?><a href="<?php echo $twitter; ?>"><i aria-hidden="true" class="fa fa-twitter twitter"></i></a><?php } ?>
+                            <?php if ( $google ) { ?><a href="<?php echo $google; ?>"><i aria-hidden="true" class="fa fa-google google"></i></a><?php } ?>
+                            <?php if ( $youtube ) { ?><a href="<?php echo $youtube; ?>"><i aria-hidden="true" class="fa fa-youtube youtube"></i></a><?php } ?>
+                            <?php if ( $instagram ) { ?><a href="<?php echo $instagram; ?>"><i aria-hidden="true" class="fa fa-instagram instagram"></i></a><?php } ?>
+                            <?php if ( $soundcloud ) { ?><a href="<?php echo $skype; ?>"><i aria-hidden="true" class="fa fa-soundcloud soundcloud"></i></a><?php } ?>
+                            <?php if ( $bandcamp ) { ?><a href="<?php echo $yelp; ?>"><i aria-hidden="true" class="fa fa-bandcamp bandcamp"></i></a><?php } ?>
+                            <?php if ( $apple ) { ?><a href="<?php echo $yelp; ?>"><i aria-hidden="true" class="fa fa-apple apple"></i></a><?php } ?>
+                        <!-- end .col-md-6 --></div>
+                    <!-- end #site-links --></div>
+                <!-- end .container-fluid --></div>
+                <div class="container-fluid" id="credits-info">
+                    <div class="row text-center">
+                        <div class="col-md-12">
+                            <p><?php echo $copyright; ?> | Site by <a href="http://prodhmd.com/" target="blank">His Master's Dance</a> | <a href="/terms-of-use/">Terms</a> | <a href="/privacy-policy/">Privacy</a> | <a href="/login/">LOGIN</a></p>
+                        <!-- end .col-md-12 --></div>
+                    <!-- end .row --></div>
+                <!-- end .container-fluid --></div>
+            <!-- end .header-table --></div>
+        <!-- end .collapse --></div>
+    <!-- end .menu --></div>                
 <!-- end #header-container --></header>
