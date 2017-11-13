da1987c4be8ed60d5ed244d82aaac384b2378c23
James Burrell <jamesburrell@SoMes-iMac.local>
2017-11-13 15:31:29 -0600

    Added get_page_slug function

--- includes/others.php
+++ includes/others.php
@@ -89,3 +89,25 @@
 			printf( '<li>%s</li>' . "\n", get_next_posts_link() );
 		echo '</ul></div>' . "\n";
 	}
+
+	/* Gets The Page Slug */
+	if ( !function_exists("get_page_slug") ) {
+		/**
+		* Returns the page or post slug.
+		* @param int|WP_Post|null $id (Optional) Post ID or post object. Defaults to global $post.
+		* @return string
+		*/
+		function get_page_slug( $id = null ){
+			$post = get_post($id);
+			if( !empty($post) ) return $post->post_name;
+			return ''; // No global $post var or matching ID available.
+		}
+		/**
+		* Display the page or post slug
+		* Uses get_page_slug() and applies 'the_slug' filter.
+		* @param int|WP_Post|null $id (Optional) Post ID or post object. Defaults to global $post.
+		*/
+		function the_slug( $id=null ){
+			echo apply_filters( 'the_slug', get_page_slug($id) );
+		}
+	}
