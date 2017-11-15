4e95f52b9d36c2706023febc340975189fee0641
lljb3 <lljb3@lljb3.com>
2017-11-15 14:00:01 +0000

    modified:   index.php

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php
-/**
- * The main template file
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file 
- *
- * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
- *
- * @package 	WordPress
- * @subpackage 	Starkers
- * @since 		Starkers 4.0
- */
+    /**
+     * The main template file
+     * This is the most generic template file in a WordPress theme
+     * and one of the two required files for a theme (the other being style.css).
+     * It is used to display a page when nothing more specific matches a query.
+     * E.g., it puts together the home page when no home.php file 
+     *
+     * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
+     *
+     * @package 	WordPress
+     * @subpackage 	Starkers
+     * @since 		Starkers 4.0
+     */
+    global $prodhmd_theme_option;
+    $slug_id = get_id_by_slug('news');
+    $slug_attachment_id = get_post_thumbnail_id($slug_id);
+    $attachment_id = get_post_thumbnail_id();
+    $bg_url = wp_get_attachment_image_src($slug_attachment_id, 'full', false);
+    $img_url = wp_get_attachment_image_src($attachment_id, 'full', false);
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header' ) ); ?>
 
@@ -54,6 +60,11 @@
         <!-- end .col-md-4 --></div>
     <!-- end .row --></div>
 <!-- end #content --></div>
+
+<!-- Background Information -->
+<div class="<?php global $post; echo get_post($post)->post_name; ?>-container" id="content-bg">
+    <img src="<?php echo $bg_url[0]; ?>" class="background img-responsive center-block" />
+<!-- end #content-bg --></div>
 
 <!-- end .blog --></main>
 
