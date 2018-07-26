495cf91f4ed083b7097193faa5ae2cdbb7fbece1
lljb3 <lljb3@lljb3.com>
2018-07-25 23:27:36 -0500

    Updates to plugin changes

--- install/installer.php
+++ install/installer.php
@@ -27,16 +27,6 @@
 				'required'  => true,
 			),
 			array(
-				'name'               => 'Contact Form Database', // The plugin name.
-				'slug'               => 'contact-form-7-to-database-extension', // The plugin slug (typically the folder name).
-				'source'             => 'https://github.com/mdsimpson/contact-form-7-to-database-extension/releases/download/2.10.31/contact-form-7-to-database-extension-2.10.31.zip', // The plugin source.
-				'required'           => false, // If false, the plugin is only 'recommended' instead of required.
-				'version'            => '2.10.30', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
-				'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-				'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-				'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			),
-			array(
 				'name'		=> 'WP Add Custom CSS',
 				'slug'		=> 'wp-add-custom-css',
 				'required'  => true,
@@ -46,7 +36,7 @@
 				'slug'               => 'LayerSlider', // The plugin slug (typically the folder name).
 				'source'             => get_template_directory_uri() . '/install/installer/plugins/layerslider.zip', // The plugin source.
 				'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-				'version'            => '6.6.1', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
+				'version'            => '6.7.6', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
 				'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 				'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 				'external_url'       => '', // If set, overrides default API URL and points to an external URL.
